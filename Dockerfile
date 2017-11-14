dcfba7fbcf2e3d4f855f978e95e36b4d81da3e7c
H2CK <H2CK@users.noreply.github.com>
2017-11-14 22:36:32 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM phusion/baseimage:latest
+
+# Set correct environment variables
+ENV DEBIAN_FRONTEND="noninteractive" HOME="/root" LC_ALL="C.UTF-8" LANG="en_US.UTF-8" LANGUAGE="en_US.UTF-8"
+
+# Use baseimage-docker's init system
+CMD ["/sbin/my_init"]
+
+ADD setup.sh /tmp/
+RUN bash /tmp/setup.sh
+       
+#Add Apache configuration
+RUN a2enmod ssl \
+	&& a2enmod proxy \
+	&& a2enmod proxy_http \
+	&& a2enmod proxy_balancer \
+	&& a2enmod lbmethod_byrequests \
+	&& a2enmod proxy_wstunnel \
+	&& a2enmod rewrite \
+	&& a2enmod headers \
+	&& a2enmod wsgi \
+	&& a2dissite 000-default 
+
+VOLUME /etc/apache2/sites-enabled /etc/letsencrypt
+
+EXPOSE 80/tcp 443/tcp
---
+++ setup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND="noninteractive"
+
+groupadd docker-data 
+usermod -a -G docker-data,adm www-data
+
+# user config
+cat <<'EOT' > /etc/my_init.d/01_user_config.sh
+#!/bin/bash
+GROUPID=${GROUP_ID:-999}
+groupmod -g $GROUPID docker-data 
+EOT
+
+# auto update
+cat <<'EOT' > /etc/my_init.d/02_auto_update.sh
+#!/bin/bash
+apt-get update -qq
+apt-get upgrade -qy
+EOT
+
+#set Port for Apache to listen to
+cat <<'EOT' > /etc/my_init.d/03_set_a2port.sh
+#!/bin/bash
+A2HTTPPORT=${HTTP_PORT:-80}
+A2HTTPSPORT=${HTTPS_PORT:-443}
+echo "Listen *:$A2HTTPPORT" > /etc/apache2/p1.conf
+echo "Listen *:$A2HTTPSPORT" > /etc/apache2/p2.conf
+cat /etc/apache2/p1.conf /etc/apache2/p2.conf > /etc/apache2/ports.conf
+rm /etc/apache2/p1.conf
+rm /etc/apache2/p2.conf
+EOT
+
+# Apache Startup Script
+mkdir -p /etc/service/apache2
+cat <<'EOT' > /etc/service/apache2/run
+#!/bin/bash
+exec /sbin/setuser root /usr/sbin/apache2ctl -DFOREGROUND 2>&1
+EOT
+
+# letencrypt renewal script
+mkdir -p /var/bin
+cat <<'EOT' > /var/bin/certbot_renew.sh
+#!/bin/bash
+/usr/bin/certbot renew
+exec /sbin/setuser root /usr/sbin/apache2ctl -k graceful -DFOREGROUND 2>&1
+EOT
+
+cat <<'EOT' > /etc/cron.d/certbot
+12 0 */10 * *	root	/var/bin/certbot_renew.sh
+EOT
+
+mkdir /usr/share/external
+chmod 777 /usr/share/external
+chmod -R +x /etc/service/ /etc/my_init.d/ /var/bin/
+
+apt-get update -qq
+apt-get upgrade -qy
+apt-get install -qy \
+    apache2 \
+    apache2-utils \
+    libexpat1 \
+    ssl-cert \
+    python \
+    libapache2-mod-wsgi \
+    python-redis \
+    python-passlib
+
+apt-get install -qy software-properties-common
+add-apt-repository ppa:certbot/certbot
+apt-get update -qq
+apt-get install -qy certbot
+
+mkdir /var/www/letsencrypt
+mkdir /var/www/letsencrypt/.well-known
+chown -R www-data:www-data /var/www/letsencrypt
+chmod -R 750 /var/www/letsencrypt/.well-known
+
+# Clean APT install files
+apt-get clean -y
+rm -rf /var/lib/apt/lists/* /var/cache/* /var/tmp/* /tmp/*
